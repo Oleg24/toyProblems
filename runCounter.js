2ae43c7c584385cfd6c1890c73f3cde1c5f97397
Oleg <o.yanchinskiy@gmail.com>
2015-04-30 14:13:54 -0700

    cleaned up run counter

--- runCounter.js
+++ runCounter.js
@@ -2,10 +2,8 @@ function runCounter(str){
   var counter = 1, result = [], letterArray = str.split('');
   for(var i = 0; i < letterArray.length; i++){
     var letter = letterArray[i];
-    console.log(letter, letterArray[i-1])
     if(letter !== letterArray[i-1]){
       for(var j = i+1; j < letterArray.length; j++){
-        console.log('j', j);
         if(letter === letterArray[j]){
           counter++; 
         }
